4c463d1a2a10eeea7b4f747e6af6a373199829ca
karolskora1993 <karol.skora@wingu.de>
2017-01-31 13:18:35 +0100

    new endpoints

---
+++ server.js
@@ -0,0 +1,79 @@
+/**
+ * Created by apple on 31.01.2017.
+ */
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongodb = require("mongodb");
+var ObjectID = mongodb.ObjectID;
+
+var USERS_COLLECTION = "users";
+
+var app = express();
+app.use(bodyParser.json());
+
+var db;
+
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  db = database;
+  console.log("Database connection ready");
+
+  var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
+
+
+/*  "/api/users/:id"
+ *    GET: find user by id
+ *    PUT: update user by id
+ *    DELETE: deletes user by id
+ */
+
+app.get("/api/users/:id", function(req, res) {
+});
+
+app.put("/api/users/:id", function(req, res) {
+});
+
+app.delete("/api/users/:id", function(req, res) {
+});
+
+
+
+/*  "/api/users"
+ *    GET: finds all users
+ *    POST: creates a new user
+ */
+
+app.get("/api/users", function(req, res) {
+  db.collection(USERS_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get contacts.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
+});
+
+app.post("/api/users", function(req, res) {
+  var newUser = req.body;
+
+  if (!req.body.name) {
+    handleError(res, "Invalid user input", "Must provide a name.", 400);
+  }
+
+  db.collection(USERS_COLLECTION).insertOne(newUser, function(err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to create new user.");
+    } else {
+      res.status(201).json(doc.ops[0]);
+    }
+  });
+});
